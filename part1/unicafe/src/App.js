5ea27c64919c22c9d1b701c7c44b90fe491e16dc
Miroslav Muras <mirekmuras@gmail.com>
2020-04-16 15:20:35 -0400

    fixed and redo step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,39 @@
 import React,{useState} from 'react';
 
-const Didplay = props => <div>{props.text} {props.value} {props.symbol}</div>
+const Header = (props) => <h2>{props.text}</h2>
+
+const Statistic = ({text,value}) => (
+  <tr>
+    <td>
+      {text} {value} 
+    </td>
+  </tr>
+)
+
+const Statistics = (props) => {
+  const {good,neutral,bad} = props.elements
+
+  const total = (good + neutral + bad)
+  const avg = (good*1+neutral*0+bad*(-1)) / total
+  const positive = (good / total) * 100 + ' %' 
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic text='Good feedback : ' value={good}/>
+          <Statistic text='Neutral feedback : ' value={neutral}/>
+          <Statistic text='Bad feedback : ' value={bad}/>
+          <Statistic text='All feedbacks : ' value={total}/>      
+          <Statistic text='Average: ' value={avg}/> 
+          <Statistic text='Positive feedbacks:' value={positive} /> 
+        </tbody>
+      </table>
+    </div>
+  )
+
+
+}
 
 const Button = (props) => {
   return (
@@ -10,30 +43,25 @@ const Button = (props) => {
   )
 }
 
-
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = (good + neutral + bad)
-  const avg = (good*1+neutral*0+bad*(-1)) / total
-  const positive = (good / total) * 100 ;
+  const elements = {good,neutral,bad}
 
   return (
     <div>
-      <h2>Give feedback</h2>
-      <Button handleClick={() => setGood(good+1)} text='Good'/>
-      <Button handleClick={() => setNeutral(neutral+1)} text='Neutral'/>
-      <Button handleClick={() => setBad(bad+1)} text='Bad'/>
-      <h2>Statistics</h2>
-      <Didplay text='Good feedback : ' value={good}/>
-      <Didplay text='Neutral feedback : ' value={neutral}/>
-      <Didplay text='Bad feedback : ' value={bad}/>
-      <Didplay text='All feedbacks : ' value={total}/>
-      <Didplay text='Average: ' value={avg}/> 
-      <Didplay text='Positive feedbacks:' value={positive} symbol='%' /> 
+      <Header text={'Give feedback'}/>
+      <Button handleClick={() => setGood(good+1)} text='Good'/> &nbsp;
+      <Button handleClick={() => setNeutral(neutral+1)} text='Neutral'/>&nbsp;
+      <Button handleClick={() => setBad(bad+1)} text='Bad'/>&nbsp;
+
+      <Header text={'Statistics'}/>
+      {(good || neutral || bad) === 0 ? 'Your feedback is needed!' : <Statistics elements={elements} /> }
+      
+
     </div>
   )
 }
